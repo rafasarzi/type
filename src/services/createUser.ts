a5e20cab969ca894c7e1bfdecddf75e1c0093e87
rafasarzi <rajustino@gmail.com>
2022-02-17 14:50:00 -0300

    change indentação

--- src/services/createUser.ts
+++ src/services/createUser.ts
@@ -1,16 +1,12 @@
 interface TechOject{
     title: string;
     experience: number;
-
-}
-
-interface CreateUserData {
+}interface CreateUserData {
     name?: string;
     email: string;
     password: string;
     techs: Array<string | TechOject>;
 }
-
 export default function createUser({name, email, password}: CreateUserData){
     const user = {
         name,
